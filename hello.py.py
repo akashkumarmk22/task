7095ff94211a60a7df4a2e6689fd983bd14594da
Akashkumarmk22 <akashkrishnamk022@gmail.com>
2024-06-07 14:41:45 +0530

    new file

---
+++ hello.py.py
@@ -0,0 +1,14 @@
+def capitalize_lines():
+    print("Enter the text : ")
+    #lines = []
+    
+    #while True:
+    line = "venusatya"
+        ##if line == "":
+            #break
+    ##lines.append(line.upper())
+    print(line.upper())
+
+ 
+# Run the function
+capitalize_lines()
